5b6dc6cbe37d108310bca91702b59c618c6049e3
Ahmed Yusuf <githubahmed1@gmail.com>
2024-12-25 11:41:02 +0300

    Set up CI/CD workflow

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+@app.route("/images")
+def images():
+    return render_template("images.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

d8d2e9574f505773233a0ae3783f110c7086e62c
Ahmed Yusuf <githubahmed1@gmail.com>
2024-12-27 19:08:57 +0300

    deploy

--- app/app.py
+++ app/app.py
@@ -4,19 +4,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    print("Rendering the home page")
     return render_template("home.html")
 
 @app.route("/about")
 def about():
+    print("Rendering the about page")
     return render_template("about.html")
 
 @app.route("/portfolio")
 def portfolio():
+    print("Rendering the portfolio page")
     return render_template("portfolio.html")
 
 @app.route("/images")
 def images():
+    print("Rendering the images page")
     return render_template("images.html")
 
 if __name__ == "__main__":
+    print("Starting the Flask application...")
     app.run(host="0.0.0.0", port=5000)
